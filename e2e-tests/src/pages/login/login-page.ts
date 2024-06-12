7bb9eee5b18401341f354f41d5a9dd45ace76439
MarcoCabort <marco.cabeza.ortega@gmail.com>
2024-06-12 20:43:22 +0200

    fill username and password

--- e2e-tests/src/pages/login/login-page.ts
+++ e2e-tests/src/pages/login/login-page.ts
@@ -1,4 +1,7 @@
+import { Locator } from "playwright";
 import {page} from "../../support/hooks"
+import { LOGIN_BUTTON_ATTR, PASSWORD_INPUT_ATTR, USERNAME_INPUT_ATTR } from "./login-page-attributes";
+
 
 const BASE_URL: string = "https://www.saucedemo.com/";
 
@@ -6,5 +9,22 @@ const BASE_URL: string = "https://www.saucedemo.com/";
 export function navigateToLoginPage(){
     page.goto(BASE_URL);
 }
+
+export async function fillUsernameInput(username:string){
+    await getUsernameInput().fill(username);
+}
+
+export async function fillPasswordInput(password:string){
+    await getPasswordInput().fill(password);
+}
 // ------------------ ASSERTIONS ------------------
+
+
 // ------------------ ELEMENTS ------------------
+function getUsernameInput(): Locator{
+    return page.locator(USERNAME_INPUT_ATTR);
+}
+
+function getPasswordInput(): Locator{
+    return page.locator(PASSWORD_INPUT_ATTR);
+}
--- e2e-tests/src/steps/login-steps.ts
+++ e2e-tests/src/steps/login-steps.ts
@@ -1,8 +1,22 @@
 import {Given, When, Then} from "@cucumber/cucumber";
-import { navigateToLoginPage } from "./login-page";
+
+import { STANDARD_USER } from "../utils/usernames";
+import { CORRECT_PASSWORD } from "../utils/passwords";
+import { navigateToLoginPage } from "../pages/login/login-page";
+
 // ------------------ GIVEN ------------------
 Given('a user that is in the login page', async function(){
     navigateToLoginPage();
 });
+
+
 // ------------------ WHEN ------------------
+When('the user introduce the standard user username', async function(){
+    await fillUsernameInput(STANDARD_USER);
+});
+
+When('the user introduce the correct password', async function(){
+    await fillPasswordInput(CORRECT_PASSWORD);
+});
+
 // ------------------ THEN ------------------
