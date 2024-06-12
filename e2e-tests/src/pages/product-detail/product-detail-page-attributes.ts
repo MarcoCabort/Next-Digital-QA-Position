4c4b92ad16eaca69245cc201dd9aad2e88a16867
MarcoCabort <marco.cabeza.ortega@gmail.com>
2024-06-12 21:10:40 +0200

    product detail page attributes

---
+++ e2e-tests/src/pages/product-detail/product-detail-page-attributes.ts
@@ -0,0 +1,4 @@
+export const PRODUCT_NAME_ATTR: string = '[data-test="inventory-item-name"]';
+export const PRODUCT_DESCRIPTION_ATTR: string = '[data-test="inventory-item-desc"]';
+export const PRODUCT_PRICE_ATTR: string = '[data-test="inventory-item-price"]';
+export const PRODUCT_DETAIL_ADD_TO_CART_BUTTON_ATTR: string = '[data-test="add-to-cart"]';
