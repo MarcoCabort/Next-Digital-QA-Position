ee03cf844d5a304fffffda058d410459eb75a75c
MarcoCabort <marco.cabeza.ortega@gmail.com>
2024-06-12 20:56:12 +0200

    products list attribute

---
+++ e2e-tests/src/pages/products-list/products-list-page-attributes.ts
@@ -0,0 +1,4 @@
+export const SHOPPING_CART_CONTAINER_ATTR: string = '#shopping_cart_container';
+export const PRODUCTS_LIST_CONTAINER_ATTR: string = '#inventory_container';
+export const MENU_BUTTON_CONTAINER_ATTR: string = '#menu_button_container';
+export const MENU_BUTTON_ATTR: string = '#react-burger-menu-btn';
