486ddc5a2d653b15b8104ab06630714fd66b287a
MarcoCabort <marco.cabeza.ortega@gmail.com>
2024-06-12 20:38:31 +0200

    import

--- e2e-tests/src/steps/login-steps.ts
+++ e2e-tests/src/steps/login-steps.ts
@@ -1,4 +1,5 @@
 import {Given, When, Then} from "@cucumber/cucumber";
+import { navigateToLoginPage } from "./login-page";
 // ------------------ GIVEN ------------------
 Given('a user that is in the login page', async function(){
     navigateToLoginPage();
